0811996610184917aa3572c29466e4f67b086395
todd-estes <elestes@gmail.com>
2024-12-20 17:33:32 -0500

    feat: Connect with django Items endpoint

--- svelte-django/src/routes/Items.svelte
+++ svelte-django/src/routes/Items.svelte
@@ -5,14 +5,15 @@
   const fetchItems = async () => {
     const response = await axios.get('http://127.0.0.1:8000/api/items/');
     items = response.data;
-    console.log(items)
   };
 
   fetchItems();
 </script>
-<h1>HELLO!!</h1>
-<ul>
-  {#each items as item}
-    <li>{item.name}: {item.description}</li>
-  {/each}
-</ul>
+
+<h1>
+  <ul>
+    {#each items as item}
+      <li>{item.name}: {item.description}</li>
+    {/each}
+  </ul>
+</h1>
